9fec6a3deb4dca69317bc7bf7e3b2c2cf2c00bd3
drobovik.yura <drobovik.yura@gmail.com>
2019-11-06 22:54:06 +0300

    SaveableListAdd

--- ShopList/app/src/main/java/com/example/shoplist/MainActivity.java
+++ ShopList/app/src/main/java/com/example/shoplist/MainActivity.java
@@ -2,39 +2,67 @@ package com.example.shoplist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 
+import com.google.gson.Gson;
+
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SharedPreferences mSettings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mSettings = getSharedPreferences("appSettings", Context.MODE_PRIVATE);
 
+        Gson gson = new Gson();
         ListView shop_list = findViewById(R.id.shop_list);
         Button add = findViewById(R.id.add);
         EditText input = findViewById(R.id.input);
 
-
         // Создаём пустой массив для хранения списка покупок
-        final ArrayList<String> list = new ArrayList<>();
+        final ArrayList<String> list;
+        String json =mSettings.getString("list","") ;
+        if (json.length() == 0) {
+
+            list = new ArrayList<>();
+
+        } else {
+
+            list = new ArrayList<>();
+            for (String s : gson.fromJson(json, String[].class)) {
+
+                list.add(s);
+
+            }
+
+        }
+
+        //list.add("Хлеб");
 
         // Создаём адаптер ArrayAdapter, чтобы привязать массив к ListView
-        final ArrayAdapter<String> adapter;
-        adapter = new ArrayAdapter<>(this,
-                android.R.layout.simple_list_item_1, list);
+        final ShopListAdapter adapter;
+        adapter = new ShopListAdapter(this, list);
         // Привяжем массив через адаптер к ListView
         shop_list.setAdapter(adapter);
 
+
         add.setOnClickListener( click -> {
-            list.add(0,input.getText().toString());
+            list.add(input.getText().toString());
             adapter.notifyDataSetChanged();
+            SharedPreferences.Editor editor = mSettings.edit();
+            editor.putString("list", gson.toJson(list) );
+            editor.apply();
             input.setText("");
         });
     }
---
+++ ShopList/app/src/main/java/com/example/shoplist/ShopListAdapter.java
@@ -0,0 +1,52 @@
+package com.example.shoplist;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.zip.Inflater;
+
+public class ShopListAdapter extends BaseAdapter {
+
+    Context context;
+    ArrayList<String> arrayList;
+    private LayoutInflater lInflater;
+
+    ShopListAdapter (Context context, ArrayList arrayList) {
+
+        this.context = context;
+        this.arrayList = arrayList;
+        this.lInflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public int getCount() {
+        return arrayList.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return arrayList.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View convertView, ViewGroup viewGroup) {
+        View view = convertView;
+        if (view == null) {
+            view = lInflater.inflate(R.layout.item_list_element, viewGroup, false);
+        }
+        TextView output = view.findViewById(R.id.textView);
+        output.setText(arrayList.get(i));
+        return view;
+    }
+}
