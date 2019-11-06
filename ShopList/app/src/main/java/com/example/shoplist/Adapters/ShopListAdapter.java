38faf8fb8cea600d7d9ce281504c9ce7fdb6e43e
SuperSLD <37121925+SuperSLD@users.noreply.github.com>
2019-11-06 23:44:45 +0300

    Разложил классы по файлам

--- ShopList/app/src/main/java/com/example/shoplist/Adapters/ShopListAdapter.java
+++ ShopList/app/src/main/java/com/example/shoplist/Adapters/ShopListAdapter.java
@@ -1,4 +1,4 @@
-package com.example.shoplist;
+package com.example.shoplist.Adapters;
 
 import android.content.Context;
 import android.view.LayoutInflater;
@@ -6,6 +6,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
+
+import com.example.shoplist.R;
 
 import java.util.ArrayList;
 import java.util.zip.Inflater;
