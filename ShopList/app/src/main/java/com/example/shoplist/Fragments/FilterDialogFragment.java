4b6f5c3a613f265fc8acfda56726c9e2dd896d44
SuperSLD <37121925+SuperSLD@users.noreply.github.com>
2019-11-13 13:00:56 +0300

    накидал основу диалога настроек

---
+++ ShopList/app/src/main/java/com/example/shoplist/Fragments/FilterDialogFragment.java
@@ -0,0 +1,38 @@
+package com.example.shoplist.Fragments;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.shoplist.Classes.NoteClass;
+import com.example.shoplist.R;
+
+import java.util.ArrayList;
+
+import androidx.fragment.app.DialogFragment;
+
+public class FilterDialogFragment extends DialogFragment {
+    private Context context;
+    private ArrayList<NoteClass> list;
+
+    public FilterDialogFragment(Context context, ArrayList list) {
+        this.context = context;
+        this.list = list;
+    }
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        getDialog().setTitle("Title!");
+        View v = inflater.inflate(R.layout.dialog_fragment_filter, null);
+        getDialog().getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+        //TODO диалог тут делается типо.
+
+        return v;
+    }
+
+}
