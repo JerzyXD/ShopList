c92eb384bbbdb33d88a4b3fd2a3fe9f400381cbd
drobovik.yura <drobovik.yura@gmail.com>
2019-12-14 22:10:39 +0300

    Сделал нормальные сслки на вк разработчиков

--- ShopList/app/src/main/java/com/example/shoplist/Activiti/SettingActivity.java
+++ ShopList/app/src/main/java/com/example/shoplist/Activiti/SettingActivity.java
@@ -1,6 +1,7 @@
 package com.example.shoplist.Activiti;
 
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
 import android.widget.TextView;
 
 import com.example.shoplist.R;
@@ -13,5 +14,11 @@ public class SettingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
         setTitle("Настройки");
+        TextView vkText1 = findViewById(R.id.textVk1);
+        TextView vkText2 = findViewById(R.id.textVk2);
+        if ((vkText1 != null) && (vkText2 != null) ){
+            vkText1.setMovementMethod(LinkMovementMethod.getInstance());
+            vkText2.setMovementMethod(LinkMovementMethod.getInstance());
+        }
     }
 }
