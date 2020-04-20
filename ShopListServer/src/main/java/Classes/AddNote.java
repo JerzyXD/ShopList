3f264a0179f54b67b3b8033b7a17f7de8d397e50
drobovik.yura <drobovik.yura@gmail.com>
2020-04-20 16:21:30 +0300

    Исправил логику добавление, редактирования зметок

--- ShopListServer/src/main/java/Classes/AddNote.java
+++ ShopListServer/src/main/java/Classes/AddNote.java
@@ -11,58 +11,52 @@ import java.sql.SQLException;
 
 public class AddNote extends HttpServlet {
     private static DBConnector connector = new DBConnector();
+    private long c;
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         PrintWriter writer = resp.getWriter();
         if (connector == null || !connector.isOpen()) connector = new DBConnector();
         if (req.getParameter("act").equals("add")) {
-            System.out.println("input note");
-            long c = connector.executeUpdate("INSERT notes VALUE('" +
-                            req.getParameter("idnote")
-                            + "','" + req.getParameter("name")
-                            + "','" + req.getParameter("type")
-                            + "','" + req.getParameter("amount")
-                            + "','" + req.getParameter("units")
-                            + "','" + req.getParameter("date")
-                            + "','" + req.getParameter("checked")
-                            + "','" + req.getParameter("iduser") + "')");
-                    writer.print(c);
-                    System.out.println("note add");
-                    System.out.println(c);
+            addNote(req, writer);
         }
 
         if (req.getParameter("act").equals("edit")) {
             System.out.println("edit note");
-            try {
-                ResultSet rs = connector.executeQuery("SELECT idnote FROM notes WHERE idnote='" + req.getParameter("idnote") + "'");
-                if (rs.next()) {
-                    long c = connector.executeUpdate("UPDATE notes SET name ='"
+                     c = connector.executeUpdate("UPDATE notes SET name ='"
                             + req.getParameter("name") + "'"
                             + "," + "type='" + req.getParameter("type") + "'"
                             + "," + "amount='" + req.getParameter("amount") + "'" + " WHERE idnote='" + req.getParameter("idnote") + "'");
+                     if (c == 0) {
+                         addNote(req, writer);
+                     }
                     writer.print(c);
                     System.out.println(c);
-                } else writer.print(0);
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
 
         }
 
         if (req.getParameter("act").equals("delete")) {
-            try {
-                ResultSet rs = connector.executeQuery("SELECT idnote FROM notes WHERE idnote='" + req.getParameter("idnote") + "'");
-                if (rs.next()) {
                     long c = connector.executeUpdate("DELETE FROM notes WHERE idnote='" + req.getParameter("idnote") + "'");
                     writer.print(c);
                     System.out.println("note delete");
                     System.out.println(c);
-                } else writer.print(0);
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
+                }
+    }
+
+    private void addNote(HttpServletRequest req, PrintWriter writer) {
+        System.out.println("input note");
+        c = connector.executeUpdate("INSERT notes VALUE('" +
+                req.getParameter("idnote")
+                + "','" + req.getParameter("name")
+                + "','" + req.getParameter("type")
+                + "','" + req.getParameter("amount")
+                + "','" + req.getParameter("units")
+                + "','" + req.getParameter("date")
+                + "','" + req.getParameter("checked")
+                + "','" + req.getParameter("iduser") + "')");
+        writer.print(c);
+        System.out.println("note add");
+        System.out.println(c);
     }
 
 
